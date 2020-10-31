97656348474cf468c70df48fdba206947178ec28
Sione Tenifa <sionetenifa@Siones-MacBook-Pro.local>
2020-10-31 07:17:48 -0600

    second

--- script.js
+++ script.js
@@ -15,22 +15,21 @@ const COLORS = [
 
 function shuffle(array) {
   let counter = array.length;
-
   while (counter > 0) {
     let index = Math.floor(Math.random() * counter);
-
     counter--;
-
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
   }
+  
+    return array;
+  }
 
-  return array;
-}
+
+
 
 let shuffledColors = shuffle(COLORS);
-
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
     const newDiv = document.createElement("div");
@@ -43,24 +42,33 @@ function createDivsForColors(colorArray) {
   }
 }
 
-let noClicking = false
+let cardOne = null;
+let cardTwo = null;
+let cardsFlipped = 0;
+let noClicking = false;
 
-function handleCardClick(event) {
-  if(noClicking){
-    console.log("FALSE")
+function handleCardClick(e) {
+
+  let currentCard = e.target;
+  currentCard.style.backgroundColor = currentCard.classList[0];
+
+  if (!cardOne || !cardTwo) {
+    currentCard.classList.add("flipped");
+    cardOne = cardOne || currentCard;
+    cardTwo = currentCard === cardOne ? null : currentCard;
   }
 
+ e.preventDefault()
+
+ let card = e.target;
+ card.style.backgroundColor = card.classList.value
 
 
-  // you can use event.target to see which element was clicked
-  event.preventDefault()
+ const clickOne = e.target.classList
 
-  // console.log(COLORS)
- const clickOne = event.target.classList.value
  console.log(clickOne)
  
  
-  // console.log("you just clicked", event.target);
 }
 
 
